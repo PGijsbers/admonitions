206be29edc4a9822b6cf858450dba315427027a6
PGijsbers <p.gijsbers@tue.nl>
2024-05-10 11:51:36 +0200

    Parse and convert most admonitions

--- gtmm_admonitions/admonition.py
+++ gtmm_admonitions/admonition.py
@@ -1,12 +1,44 @@
+import re
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.files import Files
 from mkdocs.structure.pages import Page
+
+GH_ADMONITION_HEADER_PATTERN = r"> {,3}\[!(\w+)]\s*"
+GH_ADMONITION_PATTERN = re.compile(r"> {,3}\[!\w+]\s*\n(>.*\n)+", flags=re.IGNORECASE)
+
+# TODO: Make this configurable
+ADMONITION_TYPE_MAP = {
+    "caution": "danger",
+    "important": "warning",
+}
+
+
+def convert_admonition_header(gh_header: str) -> str:
+    admonition_type_match = re.search(GH_ADMONITION_HEADER_PATTERN, gh_header, flags=re.IGNORECASE)
+    (admonition_type,) = admonition_type_match.groups()
+    mm_admonition_type = ADMONITION_TYPE_MAP.get(admonition_type.lower(), admonition_type)
+    return f"!!! {mm_admonition_type} {mm_admonition_type.title()}\n"
+
+def convert_admonition_body(gh_body: str) -> str:
+    return gh_body.replace("> ", "    ")
+
+def convert_admonition(github_admonition: str) -> str:
+    header = re.search(GH_ADMONITION_HEADER_PATTERN, github_admonition, flags=re.IGNORECASE)
+    mm_header = convert_admonition_header(header.group())
+    mm_body = convert_admonition_body(github_admonition[header.end():])
+    return mm_header + mm_body
 
 
 class AdmonitionConverter(BasePlugin):
     def on_page_markdown(
         self, markdown: str, /, *, page: Page, config: MkDocsConfig, files: Files
     ) -> str | None:
-        a = "!!! tip\n\n    This has been added by your plugin :)\n\n"
-        return a + markdown
+        """"Converts all instances of Github admonition to MM admonitions."""
+        matches = list(GH_ADMONITION_PATTERN.finditer(markdown))
+        # We traverse backwards so that the match indices stay correct
+        for match in reversed(matches):
+            mm_admonition = convert_admonition(match.group())
+            markdown = markdown[:match.start()] + mm_admonition + markdown[match.end():]
+            # breakpoint()
+        return markdown
